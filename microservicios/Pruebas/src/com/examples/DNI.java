6a2490a7c3deb7b0ae865a6f9d3d49e36ab2fdc8
joaquindag <joaquinalamogallardo@gmail.com>
2021-10-19 10:49:06 +0200

    DNI optimizado

--- microservicios/Pruebas/src/com/examples/DNI.java
+++ microservicios/Pruebas/src/com/examples/DNI.java
@@ -1,61 +1,17 @@
 package com.examples;
 
+import java.util.regex.Pattern;
 
 public class DNI {
 	
-public boolean isDNIValid(String dni){
-	String letraMayus="";
-	
-	if(dni.length()!=9||Character.isLetter(dni.charAt(8))==false) {
-		return false;
-	}
-	
-	letraMayus=dni.substring(8).toUpperCase();
-	
-	if(soloNumeros(dni)==true && letraDNI(dni).equals(letraMayus)) {
-		return true;
-	} else {
-		return false;
-	}
-	
-	
-}
-
-private boolean soloNumeros(String dni) {
-	String numero="";
-	String miDNI="";
-	String[] unoNueve= {"0","1","2","3","4","5","6","7","8","9"};
-	
-	for (int i = 0; i < dni.length()-1; i++) {
-		numero=dni.substring(i,i+1);
-		for (int j = 0; j < unoNueve.length; j++) {
-			if(numero.equals(unoNueve[j])) {
-				miDNI+=unoNueve[j];
-			}
+	public boolean isDNIValid(String dni) {
+		Pattern patron=Pattern.compile("[0-9]{7,8}[A-Z a-z]");
+		if(!patron.matcher(dni).matches()) {
+			return false;
 		}
-		
+		String letterValue=dni.substring(dni.length()-1);
+		String numberValue=dni.substring(0,dni.length()-1);
+		return Character.toUpperCase(letterValue.charAt(0))=="TRWAGMYFPDXBNJZSQVHLCKE".charAt(Integer.parseInt(numberValue)%23);
 	}
-	if(miDNI.length()!=8) {
-		return false;
-		
-	} else {
-		return true;
-	}
-}
-
-private String letraDNI(String dni) {
-	int miDNI = Integer.parseInt(dni.substring(0,8));
-	int resto = 0;
-	String miLetra = "";
-	String[] asignacionLetra = {"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"};
-
-	resto = miDNI % 23;
-
-	miLetra = asignacionLetra[resto];
-
-	return miLetra;
-}
-
-
 
 }
